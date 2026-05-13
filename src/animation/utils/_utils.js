fd858547c66f65695522be0419bacf6af3cc33be
aljames-arctic <bakanabaka.vtt@gmail.com>
2026-05-13 18:14:38 -0500

    feat: Multitarget selection
    Signed-off-by: aljames-arctic <bakanabaka.vtt@gmail.com>

---
+++ src/animation/utils/_utils.js
@@ -0,0 +1,9 @@
+import { positions } from './positions.js';
+import { teleport } from './teleport.js';
+import { text } from './text.js';
+
+export const utils = {
+    positions,
+    teleport,
+    text,
+};
