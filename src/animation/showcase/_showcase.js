14c56ef33a7d81deab18fc2552466c346ee8f08c
aljames-arctic <bakanabaka.vtt@gmail.com>
2026-05-14 15:49:53 -0500

    feat: VN Dialog box
    Signed-off-by: aljames-arctic <bakanabaka.vtt@gmail.com>

--- src/animation/showcase/_showcase.js
+++ src/animation/showcase/_showcase.js
@@ -1,9 +1,11 @@
 import { attackAttack } from "./attack-attack.js";
 import { mobPsycho } from "./mob-psycho.js";
 import { sunHaloDragon } from "./sun-halo-dragon.js";
+import { vnDialog } from "./vn-dialog.js";
 
 export const showcase = {
     attackAttack,
     mobPsycho,
-    sunHaloDragon
+    sunHaloDragon,
+    vnDialog
 }
