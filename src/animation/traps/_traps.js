36c753edc7b7915356a2be7620581c786b333c49
aljames-arctic <bakanabaka.vtt@gmail.com>
2026-05-20 09:11:01 -0500

    cleanup: Style formatting
    Signed-off-by: aljames-arctic <bakanabaka.vtt@gmail.com>

--- src/animation/traps/_traps.js
+++ src/animation/traps/_traps.js
@@ -10,6 +10,40 @@ import { projectile } from './projectile.js';
 // import { rollingBoulder } from './rolling-boulder.js';
 import { spike } from './spike.js';
 
+// High level setup function to select between different traps to configure
+async function setup (config = {}) {
+    const activeTrapKeys = Object.keys(traps).filter(key => key !== 'setup');
+    const buttons = activeTrapKeys.map(key => {
+        const locKey = `EMP.traps.${key}.title`;
+        const hasLoc = game.i18n.has(locKey);
+        const label = hasLoc ? game.i18n.localize(locKey) : key
+            .replace(/([A-Z])/g, ' $1')
+            .replace(/^./, str => str.toUpperCase())
+            .trim();
+        return { label, value: key };
+    });
+
+    const chosenTrapKey = await dialog.buttonDialog({
+        title: game.i18n.localize('EMP.traps.setup.chooseTrapTitle'),
+        buttons: buttons,
+    }, {
+        classes: ['emp-vertical-dialog'],
+        content: game.i18n.localize('EMP.traps.setup.chooseTrapContent')
+    });
+
+    if (!chosenTrapKey) {
+        ui.notifications.warn(game.i18n.localize('EMP.traps.setup.noTrapChosen'));
+        return;
+    }
+
+    const trap = traps[chosenTrapKey];
+    if (trap && typeof trap.setup === 'function') {
+        return trap.setup(config);
+    } else {
+        ui.notifications.error(game.i18n.format('EMP.traps.setup.noSetupMethod', { name: chosenTrapKey }));
+    }
+}
+
 export const traps = {
     // bullRushStatue,
     // electricDoor,
@@ -21,36 +55,5 @@ export const traps = {
     // rollingBoulder,
     spike,
 
-    setup: async function (config = {}) {
-        const activeTrapKeys = Object.keys(traps).filter(key => key !== 'setup');
-        const buttons = activeTrapKeys.map(key => {
-            const locKey = `EMP.traps.${key}.title`;
-            const hasLoc = game.i18n.has(locKey);
-            const label = hasLoc ? game.i18n.localize(locKey) : key
-                .replace(/([A-Z])/g, ' $1')
-                .replace(/^./, str => str.toUpperCase())
-                .trim();
-            return { label, value: key };
-        });
-
-        const chosenTrapKey = await dialog.buttonDialog({
-            title: game.i18n.localize('EMP.traps.setup.chooseTrapTitle'),
-            buttons: buttons,
-        }, {
-            classes: ['emp-vertical-dialog'],
-            content: game.i18n.localize('EMP.traps.setup.chooseTrapContent')
-        });
-
-        if (!chosenTrapKey) {
-            ui.notifications.warn(game.i18n.localize('EMP.traps.setup.noTrapChosen'));
-            return;
-        }
-
-        const trap = traps[chosenTrapKey];
-        if (trap && typeof trap.setup === 'function') {
-            return trap.setup(config);
-        } else {
-            ui.notifications.error(game.i18n.format('EMP.traps.setup.noSetupMethod', { name: chosenTrapKey }));
-        }
-    }
+    setup
 };
