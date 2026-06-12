df858f9a1f28d62a373f2b2ae34a107496393699
aljames-arctic <bakanabaka.vtt@gmail.com>
2026-06-12 12:05:52 -0500

    cleanup: More JK animations
    Signed-off-by: aljames-arctic <bakanabaka.vtt@gmail.com>

--- src/animation/effects/template/hit-the-dirt.js
+++ src/animation/effects/template/hit-the-dirt.js
@@ -12,11 +12,11 @@ const DEFAULT_CONFIG = {
 
 async function create(token, config, options) {
     if (options?.type == 'aefx') return;
-    const { id, template } = foundry.utils.mergeObject(DEFAULT_CONFIG, config, {inplace:false});
+    const { id, template } = foundry.utils.mergeObject(DEFAULT_CONFIG, config, { inplace: false });
 
-    const cfg = { 
-        radius: 1, 
-        icon: 'icons/magic/control/silhouette-fall-slip-prone.webp', 
+    const cfg = {
+        radius: 1,
+        icon: 'icons/magic/control/silhouette-fall-slip-prone.webp',
         label: 'Hit The Dirt!'
     };
     let [position, _] = await templates.getPosition(template, cfg);
@@ -24,70 +24,70 @@ async function create(token, config, options) {
 
     let seq = new Sequence()
         .animation()
-            .delay(100)
-            .on(token)
-            .opacity(0)
+        .delay(100)
+        .on(token)
+        .opacity(0)
 
         .effect()
-            .delay(100)
-            .file(closest("animated-spell-effects-cartoon.air.puff.01"))
-            .atLocation(token)
-            .scaleToObject(1.1)
-            .belowTokens()
-            .playbackRate(1.5)
-            .opacity(0.5)
+        .delay(100)
+        .file(closest("eskie.smoke.06.white"))
+        .atLocation(token)
+        .scaleToObject(1.1)
+        .belowTokens()
+        .playbackRate(1.5)
+        .opacity(0.5)
 
         .effect()
-            .copySprite(token)
-            .atLocation(token)
-            .scaleToObject(0.85, {considerTokenScale: true})
-            .moveTowards(position, {delay: 100, rotate: false, ease: "easeOutQuint"})
-            .duration(1600)
-            .belowTokens()
-            .filter("ColorMatrix", { saturate: -1, brightness:0 })
-            .filter("Blur", { blurX: 5, blurY: 10 })
-            .opacity(0.5)
+        .copySprite(token)
+        .atLocation(token)
+        .scaleToObject(0.85, { considerTokenScale: true })
+        .moveTowards(position, { delay: 100, rotate: false, ease: "easeOutQuint" })
+        .duration(1600)
+        .belowTokens()
+        .filter("ColorMatrix", { saturate: -1, brightness: 0 })
+        .filter("Blur", { blurX: 5, blurY: 10 })
+        .opacity(0.5)
 
         .effect()
-            .delay(900)
-            .file(closest("eskie.smoke.01.white"))
-            .atLocation(position)
-            .rotateTowards(token)
-            .scaleToObject(1.5)
-            .belowTokens()
-            .spriteOffset({x:-1.25}, {gridUnits:true})
-            .spriteRotation(-180)
-            .opacity(0.5)
+        .delay(900)
+        .file(closest("eskie.smoke.01.white"))
+        .atLocation(position)
+        .rotateTowards(token)
+        .scaleToObject(1.5)
+        .belowTokens()
+        .spriteOffset({ x: -1.25 }, { gridUnits: true })
+        .spriteRotation(-180)
+        .opacity(0.5)
 
         .effect()
-            .delay(900)
-            .file(closest("animated-spell-effects-cartoon.earth.debris.04"))
-            .atLocation(position)
-            .rotateTowards(token)
-            .scaleToObject(1.5)
-            .belowTokens()
-            .spriteOffset({x:-1.25}, {gridUnits:true})
-            .spriteRotation(90)
-            .zIndex(0)
+        .delay(900)
+        .file(closest("animated-spell-effects-cartoon.earth.debris.04"))
+        .atLocation(position)
+        .rotateTowards(token)
+        .scaleToObject(1.5)
+        .belowTokens()
+        .spriteOffset({ x: -1.25 }, { gridUnits: true })
+        .spriteRotation(90)
+        .zIndex(0)
 
         // Animate the token jumping
         .effect()
-            .copySprite(token)
-            .atLocation(token)
-            .scaleToObject(1, {considerTokenScale: true})
-            .moveTowards(position, {delay: 100, rotate: false, ease: "easeOutQuint"})    // Horizontal Movement
-            .duration(1300)
-            .animateProperty("spriteContainer", "position.y", { from: 0, to: -0.8, duration: 550, delay: 100, gridUnits:true, ease:"easeOutQuint"})
-            .animateProperty("spriteContainer", "position.y", { from: 0, to: 0.8, duration: 550, delay: 650, gridUnits:true, ease:"easeOutQuad"})
-            .animateProperty("sprite", "rotation", { from: 0, to: 90, duration: 500,delay: 100,ease:"easeOutCubic"})
-            .waitUntilFinished(-200)
+        .copySprite(token)
+        .atLocation(token)
+        .scaleToObject(1, { considerTokenScale: true })
+        .moveTowards(position, { delay: 100, rotate: false, ease: "easeOutQuint" })    // Horizontal Movement
+        .duration(1300)
+        .animateProperty("spriteContainer", "position.y", { from: 0, to: -0.8, duration: 550, delay: 100, gridUnits: true, ease: "easeOutQuint" })
+        .animateProperty("spriteContainer", "position.y", { from: 0, to: 0.8, duration: 550, delay: 650, gridUnits: true, ease: "easeOutQuad" })
+        .animateProperty("sprite", "rotation", { from: 0, to: 90, duration: 500, delay: 100, ease: "easeOutCubic" })
+        .waitUntilFinished(-200)
 
         // Update the actual token
         .animation()
-            .on(token)
-            .teleportTo(position, {relativeToCenter: true})
-            .rotate(token.document.rotation+90)
-            .opacity(1);
+        .on(token)
+        .teleportTo(position, { relativeToCenter: true })
+        .rotate(token.document.rotation + 90)
+        .opacity(1);
     return seq;
 }
 
@@ -99,8 +99,8 @@ async function play(token, config = {}) {
 function destroy(token, config = {}) {
     let seq = new Sequence()
         .animation()
-            .on(token)
-            .rotate(token.document.rotation-90);
+        .on(token)
+        .rotate(token.document.rotation - 90);
     return seq;
 }
 
