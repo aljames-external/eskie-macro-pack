25e05093ed9e798be78ac099dd2e7ca09f848852
aljames-arctic <bakanabaka.vtt@gmail.com>
2026-06-12 12:56:07 -0500

    cleanup: Petrified
    Signed-off-by: aljames-arctic <bakanabaka.vtt@gmail.com>

--- src/animation/effects/active-effect/petrified.js
+++ src/animation/effects/active-effect/petrified.js
@@ -17,32 +17,32 @@ async function create(token, config = {}) {
 
     let sequence = new Sequence()
         .effect()
-            .name(id)
-            .copySprite(token)
-            .atLocation(token)
-            .scaleToObject(1, { considerTokenScale: true })
-            .mask(token)
-            .opacity(0.4)
-            .filter("ColorMatrix", { contrast: 1, saturate: -1 })
-            .filter("Glow", { color: 0x000000, distance: 3, outerStrength: 4 })
-            .attachTo(token)
-            .fadeIn(3000)
-            .duration(5000)
-            .zIndex(1)
-            .persist()
+        .name(id)
+        .copySprite(token)
+        .atLocation(token)
+        .scaleToObject(1, { considerTokenScale: true })
+        .mask(token)
+        .opacity(0.4)
+        .filter("ColorMatrix", { contrast: 1, saturate: -1 })
+        .filter("Glow", { color: 0x000000, distance: 3, outerStrength: 4 })
+        .attachTo(token)
+        .fadeIn(3000)
+        .duration(5000)
+        .zIndex(1)
+        .persist()
 
         .effect()
-            .file(closest("https://i.imgur.com/4P2tITB.png"))
-            .name(id)
-            .atLocation(token)
-            .mask(token)
-            .opacity(1)
-            .filter("Glow", { color: 0x000000, distance: 3, outerStrength: 4 })
-            .zIndex(0)
-            .fadeIn(3000)
-            .duration(5000)
-            .attachTo(token)
-            .persist();
+        .file(closest("https://i.imgur.com/4P2tITB.png"))
+        .name(id)
+        .atLocation(token)
+        .mask(token)
+        .opacity(1)
+        .filter("Glow", { color: 0x000000, distance: 3, outerStrength: 4 })
+        .zIndex(0)
+        .fadeIn(3000)
+        .duration(5000)
+        .attachTo(token)
+        .persist();
 
     return sequence;
 }
@@ -55,17 +55,17 @@ async function play(token, config = {}) {
 async function stop(token, config = {}) {
     const mConfig = foundry.utils.mergeObject(DEFAULT_CONFIG, config, { inplace: false });
     const { id } = mConfig;
-    let sequence =  new Sequence()
+    let sequence = new Sequence()
         .effect()
-            .file(closest("animated-spell-effects-cartoon.earth.explosion.02"))
-            .atLocation(token)
-            .filter("ColorMatrix", { saturate: -1 })
-            .scaleToObject(2)
-            .randomRotation()
-            .animation()
-            .on(token)
-            .opacity(1)
-            .thenDo(() => { Sequencer.EffectManager.endEffects({ name: id, object: token }); });
+        .file(closest("jb2a.impact.earth.01.browngreen"))
+        .atLocation(token)
+        .filter("ColorMatrix", { saturate: -1 })
+        .scaleToObject(2)
+        .randomRotation()
+        .animation()
+        .on(token)
+        .opacity(1)
+        .thenDo(() => { Sequencer.EffectManager.endEffects({ name: id, object: token }); });
     await sequence.play();
 }
 
