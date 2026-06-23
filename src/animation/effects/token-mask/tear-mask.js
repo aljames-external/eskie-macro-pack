403fd967a92decf2e096739f90fd7c6e566bc7f2
Anonymous <anonymous@github.com>
2026-06-23 03:34:30 +0000

    refactor: rename deleteToken config property to deleteObject globally
    Renames the deleteToken/deleteElement configuration parameter to
    deleteObject in all individual mask effects (shatter, burn, tear, smoke)
    and the Sword Art Online death effects, completing the API generalization.

--- src/animation/effects/token-mask/tear-mask.js
+++ src/animation/effects/token-mask/tear-mask.js
@@ -2,28 +2,28 @@ import { tokenMaskEffect } from "./token-mask.js";
 
 const DEFAULT_CONFIG = {
     id: 'TearTokenMask',
-    deleteToken: false,
+    deleteObject: false,
     color: 'red',
     rotation: 0
 };
 
-async function create(token, config = {}) {
-    const { id, deleteToken, color, rotation } = foundry.utils.mergeObject(DEFAULT_CONFIG, config, { inplace: false });
+async function create(object, config = {}) {
+    const { id, deleteObject, color, rotation } = foundry.utils.mergeObject(DEFAULT_CONFIG, config, { inplace: false });
     const tokenOverlay = `eskie.wounds.token_mask.tear.01.${color}.no_base`;
     const revealOverlay = `eskie.texture_mask.tile_base.tear.01`;
-    return tokenMaskEffect.create(token, { id, deleteToken, tokenOverlay, revealOverlay, rotation });
+    return tokenMaskEffect.create(object, { id, deleteObject, tokenOverlay, revealOverlay, rotation });
 }
 
-async function play(token, config = {}) {
-    const seq = await create(token, config);
+async function play(object, config = {}) {
+    const seq = await create(object, config);
     if (seq) return seq.play();
 }
 
-async function stop(token, config = {}) {
-    const { id, deleteToken, color, rotation } = foundry.utils.mergeObject(DEFAULT_CONFIG, config, { inplace: false });
+async function stop(object, config = {}) {
+    const { id, deleteObject, color, rotation } = foundry.utils.mergeObject(DEFAULT_CONFIG, config, { inplace: false });
     const tokenOverlay = `eskie.wounds.token_mask.tear.01.${color}.no_base`;
     const revealOverlay = `eskie.texture_mask.tile_base.tear.01`;
-    return tokenMaskEffect.stop(token, { id, deleteToken, tokenOverlay, revealOverlay, rotation });
+    return tokenMaskEffect.stop(object, { id, deleteObject, tokenOverlay, revealOverlay, rotation });
 }
 
 export const tearMask = {
